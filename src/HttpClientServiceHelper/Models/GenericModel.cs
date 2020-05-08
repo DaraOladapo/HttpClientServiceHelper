4094b34cd5f7405688a73c9114a4c1c96d22ddfc
Dara Oladapo <daraoladapo@outlook.com>
2020-05-08 19:04:04 +0100

    get rettrun generic model TODO: Summary doc

---
+++ src/HttpClientServiceHelper/Models/GenericModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpClientServiceHelper.Models
+{
+    public class GenericModel<T>
+    {
+        public int StatusCode { get; set; }
+
+        public T Response { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
