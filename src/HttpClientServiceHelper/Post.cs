b82f70cfc45fa14b4ddce1fbdbbf47b126b3a9f7
Dara Oladapo <daraoladapo@outlook.com>
2020-05-17 03:22:57 +0100

    ++Generic Model Build OKay

--- src/HttpClientServiceHelper/Post.cs
+++ src/HttpClientServiceHelper/Post.cs
@@ -1,5 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using HttpClientServiceHelper.Models;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -10,11 +12,11 @@ namespace HttpClientServiceHelper
     public partial class HttpClientHelper
     {
         /// <summary>
-        /// Triggers a POST request to the specified route and retrieves the result as an HTTP Response Message.
+        /// Triggers a Post request to the specified route and retrieves the result as an HTTP Response Message
         /// </summary>
         /// <param name="Route"></param>
         /// <param name="Model"></param>
-        /// <returns>An HTTP Response Message</returns>
+        /// <returns>an HTTP Response Message</returns>
         public static async Task<HttpResponseMessage> PostAsync(string Route, object Model)
         {
             using (HttpClient httpClient = new HttpClient())
@@ -27,12 +29,12 @@ namespace HttpClientServiceHelper
             }
         }
         /// <summary>
-        /// Triggers a POST request to the specified route and retrieves the result as an HTTP Response Message.
+        /// Triggers a Post request to the specified route and retrieves the result as an HTTP Response Message
         /// </summary>
         /// <param name="Route"></param>
         /// <param name="Model"></param>
         /// <param name="Token"></param>
-        /// <returns>An HTTP Response Message</returns>
+        /// <returns>an HTTP Response Message</returns>
         public static async Task<HttpResponseMessage> PostAsync(string Route, object Model, string Token = null)
         {
             using (HttpClient httpClient = new HttpClient())
@@ -46,11 +48,59 @@ namespace HttpClientServiceHelper
             }
         }
         /// <summary>
-        /// Triggers a POST request to the specified route and retrieves the result as a string which you can serialize.
+        /// Triggers a Post request to the specified route with headers and retrieves the result as an HTTP Response Message
         /// </summary>
         /// <param name="Route"></param>
         /// <param name="Model"></param>
-        /// <returns>a string response of the HTTP Response Message</returns> 
+        /// <param name="Headers"></param>
+        /// <returns>an HTTP Response Message</returns>
+        public static async Task<HttpResponseMessage> PostAsync(string Route, object Model, List<Header> Headers)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var uri = new Uri(Route);
+                foreach (var Header in Headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(Header.Name, Header.Value);
+                }
+                string jsonTransport = JsonConvert.SerializeObject(Model);
+                var jsonPayload = new StringContent(jsonTransport, Encoding.UTF8, "application/json");
+                var httpResponse = await httpClient.PostAsync(uri, jsonPayload);
+                return httpResponse;
+            }
+        }
+        /// <summary>
+        /// Triggers a Post request to the specified route with headers and authorization and retrieves the result as an HTTP Response Message
+        /// </summary>
+        /// <param name="Route"></param>
+        /// <param name="Model"></param>
+        /// <param name="Headers"></param>
+        /// <param name="Authorization"></param>
+        /// <returns>an HTTP Response Message</returns>
+        public static async Task<HttpResponseMessage> PostAsync(string Route, object Model, List<Header> Headers, Authorization Authorization)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var uri = new Uri(Route);
+                foreach (var Header in Headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(Header.Name, Header.Value);
+                }
+                httpClient.DefaultRequestHeaders.Authorization = string.IsNullOrEmpty(Authorization.Parameter) ?
+                    new AuthenticationHeaderValue(Authorization.Scheme) : new AuthenticationHeaderValue(Authorization.Scheme, Authorization.Parameter);
+                string jsonTransport = JsonConvert.SerializeObject(Model);
+                var jsonPayload = new StringContent(jsonTransport, Encoding.UTF8, "application/json");
+                var httpResponse = await httpClient.PostAsync(uri, jsonPayload);
+                return httpResponse;
+            }
+        }
+
+        /// <summary>
+        /// Triggers a Post request to the specified route and retrieves the result as an HTTP Response Message.
+        /// </summary>
+        /// <param name="Route"></param>
+        /// <param name="Model"></param>
+        /// <returns>an HTTP Response Message as string</returns>
         public static async Task<string> PostAndGetResponseAsStringAsync(string Route, object Model)
         {
             using (HttpClient httpClient = new HttpClient())
@@ -63,12 +113,12 @@ namespace HttpClientServiceHelper
             }
         }
         /// <summary>
-        /// Triggers a POST request to the specified route and retrieves the result as a string which you can serialize.
+        /// Triggers a Post request to the specified route and retrieves the result as an HTTP Response Message.
         /// </summary>
         /// <param name="Route"></param>
         /// <param name="Model"></param>
         /// <param name="Token"></param>
-        /// <returns>a string response of the HTTP Response Message</returns> 
+        /// <returns>an HTTP Response Message as string</returns>
         public static async Task<string> PostAndGetResponseAsStringAsync(string Route, object Model, string Token = null)
         {
             using (HttpClient httpClient = new HttpClient())
@@ -79,6 +129,241 @@ namespace HttpClientServiceHelper
                 var jsonPayload = new StringContent(jsonTransport, Encoding.UTF8, "application/json");
                 var httpResponse = await httpClient.PostAsync(uri, jsonPayload);
                 return await httpResponse.Content.ReadAsStringAsync();
+            }
+        }
+        /// <summary>
+        /// Triggers a Post request to the specified route with headers and retrieves the result as an HTTP Response Message
+        /// </summary>
+        /// <param name="Route"></param>
+        /// <param name="Model"></param>
+        /// <param name="Headers"></param>
+        /// <returns>an HTTP Response Message as string</returns>
+        public static async Task<string> PostAndGetResponseAsStringAsync(string Route, object Model, List<Header> Headers)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var uri = new Uri(Route);
+                foreach (var Header in Headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(Header.Name, Header.Value);
+                }
+                string jsonTransport = JsonConvert.SerializeObject(Model);
+                var jsonPayload = new StringContent(jsonTransport, Encoding.UTF8, "application/json");
+                var httpResponse = await httpClient.PostAsync(uri, jsonPayload);
+                return await httpResponse.Content.ReadAsStringAsync();
+            }
+        }
+        /// <summary>
+        /// Triggers a Post request to the specified route with headers and authorization and retrieves the result as an HTTP Response Message
+        /// </summary>
+        /// <param name="Route"></param>
+        /// <param name="Model"></param>
+        /// <param name="Headers"></param>
+        /// <param name="Authorization"></param>
+        /// <returns>an HTTP Response Message as string</returns>
+        public static async Task<string> PostAndGetResponseAsStringAsync(string Route, object Model, List<Header> Headers, Authorization Authorization)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var uri = new Uri(Route);
+                foreach (var Header in Headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(Header.Name, Header.Value);
+                }
+                httpClient.DefaultRequestHeaders.Authorization = string.IsNullOrEmpty(Authorization.Parameter) ?
+                    new AuthenticationHeaderValue(Authorization.Scheme) : new AuthenticationHeaderValue(Authorization.Scheme, Authorization.Parameter);
+                string jsonTransport = JsonConvert.SerializeObject(Model);
+                var jsonPayload = new StringContent(jsonTransport, Encoding.UTF8, "application/json");
+                var httpResponse = await httpClient.PostAsync(uri, jsonPayload);
+                return await httpResponse.Content.ReadAsStringAsync();
+            }
+        }
+
+        /// <summary>
+        /// Triggers a Post request to the specified route and retrieves the result as an HTTP Response Message.
+        /// </summary>
+        /// <param name="Route"></param>
+        /// <param name="Model"></param>
+        /// <returns>an HTTP Response Message as GenericModel of type T</returns>
+        public static async Task<GenericModel<T>> PostAndGetResponseAsGenericModelAsync<T>(string Route, object Model)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var uri = new Uri(Route);
+                string jsonTransport = JsonConvert.SerializeObject(Model);
+                var jsonPayload = new StringContent(jsonTransport, Encoding.UTF8, "application/json");
+                var httpResponse = await httpClient.PostAsync(uri, jsonPayload);
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    if (typeof(T) == typeof(string))
+                    {
+                        return new GenericModel<T>()
+                        {
+                            Response = (T)Convert.ChangeType(await httpResponse.Content.ReadAsStringAsync(), typeof(T)),
+                            StatusCode = (int)httpResponse.StatusCode
+                        };
+                    }
+                    else
+                    {
+                        return new GenericModel<T>()
+                        {
+                            Response = JsonConvert.DeserializeObject<T>(await httpResponse.Content.ReadAsStringAsync()),
+                            StatusCode = (int)httpResponse.StatusCode
+                        };
+                    }
+                }
+                else
+                {
+                    return new GenericModel<T>()
+                    {
+                        StatusCode = (int)httpResponse.StatusCode,
+                        ErrorMessage = await httpResponse.Content.ReadAsStringAsync()
+                    };
+                }
+            }
+        }
+        /// <summary>
+        /// Triggers a Post request to the specified route and retrieves the result as an HTTP Response Message.
+        /// </summary>
+        /// <param name="Route"></param>
+        /// <param name="Model"></param>
+        /// <param name="Token"></param>
+        /// <returns>an HTTP Response Message as GenericModel of type T</returns>
+        public static async Task<GenericModel<T>> PostAndGetResponseAsGenericModelAsync<T>(string Route, object Model, string Token = null)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var uri = new Uri(Route);
+                httpClient.DefaultRequestHeaders.Authorization = string.IsNullOrEmpty(Token) ? new AuthenticationHeaderValue("Bearer", Token) : null;
+                string jsonTransport = JsonConvert.SerializeObject(Model);
+                var jsonPayload = new StringContent(jsonTransport, Encoding.UTF8, "application/json");
+                var httpResponse = await httpClient.PostAsync(uri, jsonPayload);
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    if (typeof(T) == typeof(string))
+                    {
+                        return new GenericModel<T>()
+                        {
+                            Response = (T)Convert.ChangeType(await httpResponse.Content.ReadAsStringAsync(), typeof(T)),
+                            StatusCode = (int)httpResponse.StatusCode
+                        };
+                    }
+                    else
+                    {
+                        return new GenericModel<T>()
+                        {
+                            Response = JsonConvert.DeserializeObject<T>(await httpResponse.Content.ReadAsStringAsync()),
+                            StatusCode = (int)httpResponse.StatusCode
+                        };
+                    }
+                }
+                else
+                {
+                    return new GenericModel<T>()
+                    {
+                        StatusCode = (int)httpResponse.StatusCode,
+                        ErrorMessage = await httpResponse.Content.ReadAsStringAsync()
+                    };
+                }
+            }
+        }
+        /// <summary>
+        /// Triggers a Post request to the specified route with headers and retrieves the result as an HTTP Response Message
+        /// </summary>
+        /// <param name="Route"></param>
+        /// <param name="Model"></param>
+        /// <param name="Headers"></param>
+        /// <returns>an HTTP Response Message as GenericModel of type T</returns>
+        public static async Task<GenericModel<T>> PostAndGetResponseAsGenericModelAsync<T>(string Route, object Model, List<Header> Headers)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var uri = new Uri(Route);
+                foreach (var Header in Headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(Header.Name, Header.Value);
+                }
+                string jsonTransport = JsonConvert.SerializeObject(Model);
+                var jsonPayload = new StringContent(jsonTransport, Encoding.UTF8, "application/json");
+                var httpResponse = await httpClient.PostAsync(uri, jsonPayload);
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    if (typeof(T) == typeof(string))
+                    {
+                        return new GenericModel<T>()
+                        {
+                            Response = (T)Convert.ChangeType(await httpResponse.Content.ReadAsStringAsync(), typeof(T)),
+                            StatusCode = (int)httpResponse.StatusCode
+                        };
+                    }
+                    else
+                    {
+                        return new GenericModel<T>()
+                        {
+                            Response = JsonConvert.DeserializeObject<T>(await httpResponse.Content.ReadAsStringAsync()),
+                            StatusCode = (int)httpResponse.StatusCode
+                        };
+                    }
+                }
+                else
+                {
+                    return new GenericModel<T>()
+                    {
+                        StatusCode = (int)httpResponse.StatusCode,
+                        ErrorMessage = await httpResponse.Content.ReadAsStringAsync()
+                    };
+                };
+            }
+        }
+        /// <summary>
+        /// Triggers a Post request to the specified route with headers and authorization and retrieves the result as an HTTP Response Message
+        /// </summary>
+        /// <param name="Route"></param>
+        /// <param name="Model"></param>
+        /// <param name="Headers"></param>
+        /// <param name="Authorization"></param>
+        /// <returns>an HTTP Response Message as GenericModel of type T</returns>
+        public static async Task<GenericModel<T>> PostAndGetResponseAsGenericModelAsync<T>(string Route, object Model, List<Header> Headers, Authorization Authorization)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var uri = new Uri(Route);
+                foreach (var Header in Headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(Header.Name, Header.Value);
+                }
+                httpClient.DefaultRequestHeaders.Authorization = string.IsNullOrEmpty(Authorization.Parameter) ?
+                    new AuthenticationHeaderValue(Authorization.Scheme) : new AuthenticationHeaderValue(Authorization.Scheme, Authorization.Parameter);
+                string jsonTransport = JsonConvert.SerializeObject(Model);
+                var jsonPayload = new StringContent(jsonTransport, Encoding.UTF8, "application/json");
+                var httpResponse = await httpClient.PostAsync(uri, jsonPayload);
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    if (typeof(T) == typeof(string))
+                    {
+                        return new GenericModel<T>()
+                        {
+                            Response = (T)Convert.ChangeType(await httpResponse.Content.ReadAsStringAsync(), typeof(T)),
+                            StatusCode = (int)httpResponse.StatusCode
+                        };
+                    }
+                    else
+                    {
+                        return new GenericModel<T>()
+                        {
+                            Response = JsonConvert.DeserializeObject<T>(await httpResponse.Content.ReadAsStringAsync()),
+                            StatusCode = (int)httpResponse.StatusCode
+                        };
+                    }
+                }
+                else
+                {
+                    return new GenericModel<T>()
+                    {
+                        StatusCode = (int)httpResponse.StatusCode,
+                        ErrorMessage = await httpResponse.Content.ReadAsStringAsync()
+                    };
+                }
             }
         }
     }
